9a5ffb54a2a65dd8b9417c39bd0e960d1b375f57
Shany Shmuely <shany.shmuely@ironsrc.com>
2021-03-29 10:05:57 +0300

    ICA input

--- ICA_input.m
+++ ICA_input.m
@@ -3,19 +3,12 @@ clear all; close all; clc;
 % ICA input
 
 n = 10;
-
 v = rand(n, 1);
-
 coef = 12*power(n,2) / ((n-1)*(n-2)*(n-3));
-
 v_squred_diag = diag(power(v,2));
-
 A = coef * (((n+1)/n) * v_squred_diag - ((n-1)/power(n,2)) * trace(v_squred_diag) * eye(n));
-
 u_coef = coef * ((2*n-2)/power(n,2));
-
 sqrt_u_coef = sqrt(u_coef);
-
 u = sqrt_u_coef * v;
 
 % send to ricatti as input (A, u) for W = A - uu' as W of weighted SVD
