7ca44776b75ac8fb718c98ada0a12dfa4db91fc0
Shany Shmuely <shany.shmuely@ironsrc.com>
2022-01-16 15:31:56 +0200

    add inv sqrt

--- experiments/krylov_vs_ricatti.m
+++ experiments/krylov_vs_ricatti.m
@@ -1,56 +1,121 @@
 clear all; close all; clc;
 RandStream.setGlobalStream(RandStream('mt19937ar','seed', 12121));  % fixed seed
 
-global pertubation_sign;
-pertubation_sign = 1;
-
 n = 100;                    % num of samples
-dd = rand(n, 1);          
 k = 1;                      % low rank pert dim
-Z = rand(n,k);
-A = diag(dd) + Z * Z';
+Z = randn(n,k);
 
-min_rank = 2;
-max_rank = 10;
+%todo rank 0
+min_rank = 1;
+max_rank = 6;
 update_rank_list = linspace(min_rank, max_rank, max_rank-min_rank+1);
 
-true_value = sqrtm(A);
+% graph params - need to change only here
+optimization_function = 'sqrt'; % 'sqrt'|'inv_sqrt'
+diagonal_dist = 'uniform'; % 'uniform'|'logspace'
 
-ricatti_err_values = [];
-krylov_err_values = [];
 
-fun_mat_sqrt = @(X) sqrtm(X);
 
-for rank = update_rank_list
-    ricatti_update = get_ricatti_approx(n, dd, Z, rank);
-    ricatti_approx = diag(sqrt(dd)) + ricatti_update*ricatti_update';
-    ricatti_err =  get_norm_diff(true_value, ricatti_approx);
-    ricatti_err_values(end+1) = ricatti_err;
-    
-    krylov_update = get_krylov_approx(fun_mat_sqrt, n, dd, Z, rank);
-    krylov_approx = diag(sqrt(dd)) + krylov_update;
-    krylov_err =  get_norm_diff(true_value, krylov_approx);
-    krylov_err_values(end+1) = krylov_err;
+
+[fun_mat_sqrt, dd] = get_graph_params(n, optimization_function, diagonal_dist);
+get_diff_graph(n, dd, Z, fun_mat_sqrt, optimization_function, update_rank_list)
+
+
+
+%% ------------------------- methods ------------------------- %%
+function [fun_mat_sqrt, dd] = get_graph_params(n, optimization_function, diagonal_dist)
+    switch optimization_function
+        case 'sqrt'
+            fun_mat_sqrt = @(X) sqrtm(X);
+        case 'inv_sqrt'
+            fun_mat_sqrt = @(X) inv(sqrtm(X));
+        otherwise
+            error('not supportive opt function');
+    end
+
+    switch diagonal_dist
+        case 'uniform'
+            dd = rand(n, 1);  
+        case 'logspace'
+            dd = logspace(-3, 3, n)';
+        otherwise
+            error('not supportive diagonal_dist');
+    end
 end
 
-fprintf("krylov_err_values:");
-disp(krylov_err_values);
+function result_diag = get_diag(optimization_function, dd)
+    switch optimization_function
+        case 'sqrt'
+            result_diag = diag(sqrt(dd));
+        case 'inv_sqrt'
+            result_diag = diag(dd.^(-1/2));
+        otherwise
+            error('not supportive opt function');
+    end
+end
 
-fprintf("ricatti_err_values:");
-disp(ricatti_err_values);
 
-plot_graphs(update_rank_list, ricatti_err_values, krylov_err_values);
+function get_diff_graph(n, dd, Z, func, optimization_function, update_rank_list)
+    
+    A = diag(dd) + Z * Z';
+    
+%     A = diag(dd) - Z * Z'; only in sqrt
+
+    
+    true_value = func(A);
+    ricatti_err_values = [];
+    krylov_err_values = [];
+
+    result_diag = get_diag(optimization_function, dd);
+    global pertubation_sign;
+    if (strcmp(optimization_function, 'sqrt')); pertubation_sign = 1; else; pertubation_sign = -1; end
+    
+    for rank = update_rank_list
+        ricatti_update = get_ricatti_approx(n, Z, rank, result_diag, optimization_function);
+        ricatti_approx = result_diag + pertubation_sign * (ricatti_update*ricatti_update');
+        ricatti_err =  get_norm_diff(true_value, ricatti_approx);
+        ricatti_err_values(end+1) = ricatti_err;
+
+        krylov_update = get_krylov_approx(func, n, dd, Z, rank);
+        krylov_approx = result_diag + krylov_update;
+        krylov_err =  get_norm_diff(true_value, krylov_approx);
+        krylov_err_values(end+1) = krylov_err;
+    end
+
+    fprintf("krylov_err_values:");
+    disp(krylov_err_values);
+
+    fprintf("ricatti_err_values:");
+    disp(ricatti_err_values);
+
+    plot_graphs(update_rank_list, ricatti_err_values, krylov_err_values);
+
+end
+
 
 
 function diff = get_norm_diff(H_true, H_approx)
     diff = norm(H_approx-H_true, 'fro') / norm(H_true, 'fro');
+%     diff = norm(H_approx-H_true) / norm(H_true);
+
 end
 
+function res = get_C_ricatti(result_diag, Z, optimization_function)
+    switch optimization_function
+        case 'sqrt'
+            res = Z;
+        case 'inv_sqrt'
+            inv_D = result_diag*result_diag;
+            res = inv_D * Z * sqrtm(inv(eye(size(Z,2)) + Z'*inv_D*Z));
+        otherwise
+            error('not supportive opt function');
+    end
+end
 
-function ricatti_update = get_ricatti_approx(n, dd, Z, rank)
-    A_ricatti = diag(sqrt(dd));
+function ricatti_update = get_ricatti_approx(n, Z, rank, result_diag, optimization_function)
+    A_ricatti = result_diag;
     B_ricatti = speye(n);
-    C_ricatti = Z';
+    C_ricatti = get_C_ricatti(result_diag, Z, optimization_function)';
     
     params = get_ricatti_params(rank);
     [X_Riemannian, ~] =  Riemannian_lowrank_riccati(A_ricatti, B_ricatti, C_ricatti, params);
@@ -102,7 +167,7 @@ end
 
 function params = get_ricatti_params(rank)
     params.rmax = rank; % Maximum rank
-    params.tol_rel = 1e-6; % Stopping criterion for rank incrementating procedure
+    params.tol_rel = 1e-13; % Stopping criterion for rank incrementating procedure
     params.tolgradnorm = 1e-10; % Stopping for fixed-rank optimization
     params.maxiter = 100; % Number of iterations for fixed-rank optimization
     params.maxinner = 30; % Number of trust-region subproblems for fixed-rank optimization
@@ -112,7 +177,6 @@ end
 
 function plot_graphs(update_rank_list, ricatti_err_values, krylov_err_values)
 
-    
     hold on;
     plot(update_rank_list, ricatti_err_values, '-', 'Color', 'red', 'LineWidth',2, 'DisplayName','ricatti err')
     grid on;   
@@ -121,15 +185,14 @@ function plot_graphs(update_rank_list, ricatti_err_values, krylov_err_values)
     plot(update_rank_list, krylov_err_values, '-', 'Color', 'blue', 'LineWidth',2, 'DisplayName','krylov err')
     grid on;    
 
-
-
-    title('Results ricatti vs. Krylov', 'FontSize',16)
+    title('Ricatti vs. Krylov', 'FontSize',16)
     ylabel('Diff - approx. vs. true value in fro norm')
     xlabel('update rank')
     ax = gca;
 %     set(gca, 'XScale', 'log')
+    % todo - ylim 10^0
     ax.XAxisLocation = 'origin';
-%     set(gca, 'YScale', 'log')
+    set(gca, 'YScale', 'log')
     ax.YAxisLocation = 'origin';
     legend;
 %     legend(ca, 'Location', 'southwest')
